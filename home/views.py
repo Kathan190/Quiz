df4f93ff143176e88218350d460d2c30f75e0fcf
iamkathan <shethkathan4@gmail.com>
2021-04-01 21:58:49 +0100

    message part is done

--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,HttpResponse
 from home.models import Signup
 from home.models import Contact
 from home.models import Login
+from django.contrib import messages
 # Create your views here.
 
 def home(request):
@@ -11,20 +12,24 @@ def home(request):
             email = request.POST.get('email')
             psw = request.POST.get('psw')
             psw_repeat = request.POST.get('psw_repeat')
-            if psw == psw_repeat:
-                signup = Signup(email=email, psw=psw, psw_repeat=psw_repeat)
-                signup.save()  
+            print(email,psw,psw_repeat)
+
+            if len(email)<5 or len(psw)<5 or (psw_repeat == psw):
+                messages.error(request, 'Please fill the form correctly')
             else:
-                return HttpResponse("Password is incorrect")
+                signup = Signup(email=email,psw=psw,psw_repeat=psw_repeat)
+                signup.save()
+                messages.success(request, "Your account is successfully created")
+
         
         #contact database code
-        if request.POST.get("Contact") == "Contact":
-            email = request.POST.get('email1')
-            contact = request.POST.get('contact')
-            subject = request.POST.get('subject')
-            con = Contact(email=email,contact=contact,subject=subject)
-            con.save()
-
+            if request.POST.get("Contact") == "Contact":
+                email = request.POST.get('email1')
+                contact = request.POST.get('contact')
+                subject = request.POST.get('subject')
+                con = Contact(email=email,contact=contact,subject=subject)
+                con.save()
+    
         #login database code
         if request.POST.get("Login") == "Login":
             email = request.POST.get('email')
